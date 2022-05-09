229c811d5535022d8b6bb9bdc255ff218b0aa7f5
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-09 14:44:19 -0400

    codewars function 1

--- codewars.js
+++ codewars.js
@@ -2,7 +2,13 @@
 // Testing 1-2-3
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
 
-
+let number = function(array){
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+      result.push(`${i + 1}: ${array[i]}`);
+    }
+    return result;
+  }
 
 
 // # 2
