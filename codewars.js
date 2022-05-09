6cd993afa89803ece226717fb04903146a0b62e5
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-09 15:26:46 -0400

    codewars function 3

--- codewars.js
+++ codewars.js
@@ -22,7 +22,17 @@ const arr = N => N === undefined ? [] : [...Array(N).keys()];
 // Drone Fly-By
 // https://www.codewars.com/kata/58356a94f8358058f30004b5/train/javascript
 
-
+function flyBy(lamps, drone){
+    let lampL = lamps.length;
+    let droneL = drone.length;
+    
+    if (droneL > lampL) {
+      return 'o'.repeat(lampL);
+    }
+    else {
+      return 'o'.repeat(droneL) + 'x'.repeat(lampL - droneL);
+    }
+  }
 
 
 // # 4
